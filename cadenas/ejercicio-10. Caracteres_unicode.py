6d3d74743ff799e2d4d25c9265bfff54c797cfee
Ana  Dieguez <adiejul560@g.educaand.es>
2025-10-25 22:54:31 +0200

    organizo la frase con los chr, y queda la franse con emoticonos

---
+++ cadenas/ejercicio-10. Caracteres_unicode.py
@@ -0,0 +1,9 @@
+ 
+print (
+    chr(0x1f44f), chr(0x1f44f),
+    "ENHORABUENA!!",
+    chr(0x1f38a), chr(0x1f389),
+    "has acabado la primera ronda de ejercicios de Python,"
+    "ya puedes ir a jugar con tus amigos",
+     chr(0x1f46d), chr(0x1f46b),
+)
