67c5ecc857deb2016caca343e6c474830cbf221f
Ana  Dieguez <adiejul560@g.educaand.es>
2025-10-25 22:28:41 +0200

    ejercicio 8, he definido la altura con float,ya que al haber decimales, es como se definie. y en print he he puesto altura:6.3f, ya que 6 significa cuantos digitos puede tener en total y el 3 es para definir cuantos decimales maximos esta permitido

--- numeros/ejercicio-9. Mi_primera_f-string.py
+++ numeros/ejercicio-9. Mi_primera_f-string.py
@@ -1,4 +1,4 @@
-nombre = "pepe"
-edad = "20"
-
-print(f"{nombre} tiene {edad} años.")
+nombre = input("¿como te llamas?")
+edad = int(input("¿Que edad tienes?"))
+altura = float(input("¿Cuanto mides?, en metros"))
+print(f"Te llamas {nombre} tienes {edad} años y mides {altura:6.3f}.")
