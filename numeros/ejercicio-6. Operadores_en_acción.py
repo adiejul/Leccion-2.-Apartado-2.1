effbb7d2e0e2af0993889feaeb410175b50e991f
Ana  Dieguez <adiejul560@g.educaand.es>
2025-10-25 19:00:53 +0200

    ejercicio 6, he añadido la multiplicacion, resta y division de a y b

--- numeros/ejercicio-6. Operadores_en_acción.py
+++ numeros/ejercicio-6. Operadores_en_acción.py
@@ -2,5 +2,11 @@ a= int(input("Introduce el primer número"))
 b= int(input("Introduce el segundo número"))
 
 suma= a + b
+resta= a - b
+multiplicacion= a * b
+division= a / b
 
 print("la suma de ", a, "+", b, "es", suma)
+print("la resta de ", a, "-", b, "es", resta)
+print("la multiplicacion de ", a, "x", b, "es", multiplicacion)
+print("la división de ", a, "/", b, "es", division)
