b66f1dc32e546b3aeaf47a0566ff42dac6ec3d94
Ana  Dieguez <adiejul560@g.educaand.es>
2025-10-25 18:48:43 +0200

    ejercicio 5 segunda parte, añado tambien int() para que lo coja como numero  no como cadena. y el resultado es que arriba junta 5 y 50 = 510 y abajo suma 5 y 10= 15

--- numeros/ejercicio-5. Sumadora_mínima.py
+++ numeros/ejercicio-5. Sumadora_mínima.py
@@ -2,3 +2,9 @@ numA= input("Introduce el número A: ")
 numB= input("Introduce el número B: ")
 suma= (numA+numB)
 print("la suma es :", suma)
+
+
+numA= int(input("Introduce el número A: "))
+numB= int(input("Introduce el número B: "))
+suma= (numA+numB)
+print("la suma es :", suma)
