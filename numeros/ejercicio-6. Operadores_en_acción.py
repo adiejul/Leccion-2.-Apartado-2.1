16f67b92f412279a0408c2e7066afba77cc3f686
Ana  Dieguez <adiejul560@g.educaand.es>
2025-10-25 19:08:00 +0200

    ejercicio 6, añado la division, el entero de la division, el resto, y la potencia entre a y b

--- numeros/ejercicio-6. Operadores_en_acción.py
+++ numeros/ejercicio-6. Operadores_en_acción.py
@@ -1,12 +1,18 @@
-a= int(input("Introduce el primer número"))
-b= int(input("Introduce el segundo número"))
+a= int(input("Introduce el primer número "))
+b= int(input("Introduce el segundo número "))
 
 suma= a + b
 resta= a - b
 multiplicacion= a * b
 division= a / b
+divEnt= a//b
+resto= a%b
+potencia= a**b
 
 print("la suma de ", a, "+", b, "es", suma)
 print("la resta de ", a, "-", b, "es", resta)
 print("la multiplicacion de ", a, "x", b, "es", multiplicacion)
 print("la división de ", a, "/", b, "es", division)
+print("la division sin decimales de ", a, "/", b, "es", divEnt)
+print("el resto de ", a, "y", b, "es", resto)
+print("la potencia de ", a, "^", b, "es", potencia)
