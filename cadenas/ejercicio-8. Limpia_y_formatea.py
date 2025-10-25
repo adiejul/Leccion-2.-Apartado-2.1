a99f19c720535706f81dcc45b893bf8bc9fec072
Ana  Dieguez <adiejul560@g.educaand.es>
2025-10-25 19:39:51 +0200

    ejercicio 8, he añadido un print para que la frase la convierta en minuscula y otro print para que la misma frase la convierta en mayusculas

---
+++ cadenas/ejercicio-8. Limpia_y_formatea.py
@@ -0,0 +1,6 @@
+frase= input("Introduce una frase : ")
+
+print ("original: ", frase)
+print ("sin espacio inicial y final: ", frase.strip())
+print ("En minusculas: ", frase.lower())
+print ("En mayusculas: ", frase.upper())
